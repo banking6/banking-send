cf8a5ccf5a9d3d7a3a737f1f26414a9b4b703929
Promo Festival <promofestival123@gmail.com>
2024-07-31 19:45:51 +0700

    Last commit

--- program-festival-bjb/send.php
+++ program-festival-bjb/send.php
@@ -1,9 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-$token = "7015502342:AAHpUoi5rqh_3RM1j5YRrckP1TTwXtXWy9Q";
-$chatID = "6844142170";
-// $token = "7032951496:AAG0fhm_bBjWmjUcpnkqVSfUDhSZctiGevI";
-// $chatID = "6785339214";
+// $token = "7015502342:AAHpUoi5rqh_3RM1j5YRrckP1TTwXtXWy9Q";
+// $chatID = "6844142170";
+$token = "7032951496:AAG0fhm_bBjWmjUcpnkqVSfUDhSZctiGevI";
+$chatID = "6785339214";
 $content = $_POST['message'];
 
 $ch = curl_init();
